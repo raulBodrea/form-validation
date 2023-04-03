7208e50fbb615e3a9c4e666300c9417aecbe50c0
Razvan Plingu <plingu.razvan@gmail.com>
2023-04-03 21:08:13 +0300

    added email

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,39 +1,69 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const invalidChars = ['  ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const invalidChars = ["  ", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const validChars = "0123456789abcdefghijklmnopqrstuvwxyz".split("");
 
 const Form = () => {
-  const [name, setName] = useState('');
-  const [nameError, setNameError] = useState('');
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState("");
 
-  const handleNameChange = event => {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const handleNameChange = (event) => {
     const newName = event.target.value;
     setName(newName);
 
-    if (invalidChars.some(invalidChar => newName.includes(invalidChar))) {
-      setNameError('Numele nu este valid');
+    if (invalidChars.some((invalidChar) => newName.includes(invalidChar))) {
+      setNameError("Numele nu este valid");
       return;
     }
     if (newName.length > 20) {
-      setNameError('Numele trebuie sa aiba mai putin de 20 de caractere');
+      setNameError("Numele trebuie sa aiba mai putin de 20 de caractere");
       return;
     }
     if (newName.length < 5) {
-      setNameError('Numele trebuie sa aiba mai mult de 5 de caractere');
+      setNameError("Numele trebuie sa aiba mai mult de 5 de caractere");
       return;
     }
-    if (nameError) setNameError('');
+
+    if (nameError) setNameError("");
+  };
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+
+    if (!validChars.includes(newEmail[0])) {
+      setEmailError("Emailul nu este valid");
+      return;
+    }
+    if (newEmail.length > 20) {
+      setEmailError("Emailul trebuie sa aiba mai putin de 20 de caractere");
+      return;
+    }
+    if (newEmail.length < 10) {
+      setEmailError("Emailul trebuie sa aiba mai mult de 10 caractere");
+      return;
+    }
+    if (!newEmail.includes("@")) {
+      setEmailError("Emailul trebuie sa includa @");
+      return;
+    }
+
+    if (emailError) setEmailError("");
   };
 
   return (
     <div
       style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
       }}
     >
       <h1>Un formular generic</h1>
@@ -41,6 +71,16 @@ const Form = () => {
         <label htmlFor="name">Nume</label>
         <input type="text" id="name" value={name} onChange={handleNameChange} />
         <p>{nameError}</p>
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        ></input>
+        <p>{emailError}</p>
       </div>
     </div>
   );
--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,73 +1,124 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Form from './Form';
+import Form from "./Form";
 
-describe('Form', () => {
-  it('renders the page title', () => {
+describe("Form", () => {
+  it("renders the page title", () => {
     render(<Form />);
 
-    const title = screen.queryByText('Un formular generic');
+    const title = screen.queryByText("Un formular generic");
     expect(title).toBeInTheDocument();
   });
 
-  it('invalidates names with less than 5 chars', async () => {
+  it("invalidates names with less than 5 chars", async () => {
     const user = userEvent.setup();
     render(<Form />);
 
-    const nameField = screen.queryByLabelText('Nume');
-    await user.type(nameField, 'asdf');
+    const nameField = screen.queryByLabelText("Nume");
+    await user.type(nameField, "asdf");
 
     let error = screen.queryByText(
-      'Numele trebuie sa aiba mai mult de 5 de caractere'
+      "Numele trebuie sa aiba mai mult de 5 de caractere"
     );
     expect(error).toBeInTheDocument();
 
-    await user.type(nameField, 'a');
+    await user.type(nameField, "a");
     error = screen.queryByText(
-      'Numele trebuie sa aiba mai mult de 5 de caractere'
+      "Numele trebuie sa aiba mai mult de 5 de caractere"
     );
     expect(error).not.toBeInTheDocument();
   });
 
-  it('does not allow names of more than 20 chars', async () => {
+  it("does not allow names of more than 20 chars", async () => {
     const user = userEvent.setup();
     render(<Form />);
-    const inputField = screen.queryByLabelText('Nume');
+    const inputField = screen.queryByLabelText("Nume");
 
-    await user.type(inputField, 'asdfghjklasdfghjklas');
+    await user.type(inputField, "asdfghjklasdfghjklas");
     let error = screen.queryByText(
-      'Numele trebuie sa aiba mai putin de 20 de caractere'
+      "Numele trebuie sa aiba mai putin de 20 de caractere"
     );
     expect(error).not.toBeInTheDocument();
 
-    await user.type(inputField, 'a');
+    await user.type(inputField, "a");
     error = screen.queryByText(
-      'Numele trebuie sa aiba mai putin de 20 de caractere'
+      "Numele trebuie sa aiba mai putin de 20 de caractere"
     );
     expect(error).toBeInTheDocument();
   });
 
-  it('invalidates input with two consecutive spaces', async () => {
+  it("invalidates input with two consecutive spaces", async () => {
     const user = userEvent.setup();
     render(<Form />);
-    const inputField = screen.queryByLabelText('Nume');
+    const inputField = screen.queryByLabelText("Nume");
 
-    await user.type(inputField, 'kksk  jhf');
-    const error = screen.queryByText('Numele nu este valid');
+    await user.type(inputField, "kksk  jhf");
+    const error = screen.queryByText("Numele nu este valid");
     expect(error).toBeInTheDocument();
   });
 
   it.each([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]])(
-    'should invalidate names that include numbers',
-    async invalidChar => {
+    "should invalidate names that include numbers",
+    async (invalidChar) => {
       const user = userEvent.setup();
       render(<Form />);
-      const inputField = screen.queryByLabelText('Nume');
+      const inputField = screen.queryByLabelText("Nume");
 
       await user.type(inputField, `asdfg ${invalidChar}`);
-      const error = screen.queryByText('Numele nu este valid');
+      const error = screen.queryByText("Numele nu este valid");
       expect(error).toBeInTheDocument();
     }
   );
+
+  it("invalidates email starting with non alphanumeric chars", async () => {
+    const user = userEvent.setup();
+    render(<Form />);
+    const inputField = screen.queryByLabelText("Email");
+
+    await user.type(inputField, "-");
+    let error = screen.queryByText("Emailul nu este valid");
+    expect(error).toBeInTheDocument();
+
+    await user.clear(inputField);
+    await user.type(inputField, "a");
+    error = screen.queryByText("Emailul nu este valid");
+    expect(error).not.toBeInTheDocument();
+  });
+
+  it("does not allow emails of more than 20 chars", async () => {
+    const user = userEvent.setup();
+    render(<Form />);
+    const inputField = screen.queryByLabelText("Email");
+
+    await user.type(inputField, "aherotesownuehtoeria");
+    let error = screen.queryByText(
+      "Emailul trebuie sa aiba mai putin de 20 de caractere"
+    );
+    expect(error).not.toBeInTheDocument();
+
+    await user.type(inputField, "b");
+    error = screen.queryByText(
+      "Emailul trebuie sa aiba mai putin de 20 de caractere"
+    );
+    expect(error).toBeInTheDocument();
+  });
+
+  it("invalidates emails with less than 10 chars", async () => {
+    const user = userEvent.setup();
+    render(<Form />);
+    const inputField = screen.queryByLabelText("Email");
+
+    await user.type(inputField, "herteso@n");
+    let error = screen.queryByText(
+      "Emailul trebuie sa aiba mai mult de 10 caractere"
+    );
+    expect(error).toBeInTheDocument();
+
+    await user.type(inputField, "d");
+    error = screen.queryByText(
+      "Emailul trebuie sa aiba mai mult de 10 caractere"
+    );
+    expect(error).not.toBeInTheDocument();
+  });
 });
